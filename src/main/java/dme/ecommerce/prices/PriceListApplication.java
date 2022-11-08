a8558bb788be7f8551e6d2a65ba88329b2618e70
David Molina <david.molina.estrada@gmail.com>
2022-11-08 19:40:59 +0100

    Init commit with generate spring project

---
+++ src/main/java/dme/ecommerce/prices/PriceListApplication.java
@@ -0,0 +1,13 @@
+package dme.ecommerce.prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dme/ecommerce/prices/PriceListApplicationTests.java
@@ -0,0 +1,13 @@
+package dme.ecommerce.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
